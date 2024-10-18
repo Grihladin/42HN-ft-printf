ebc569a26e60e8b93adad6b6299b37adbdae9a97
mratke <mratke@student.42heilbronn.de>
2024-10-18 15:48:03 +0200

    now norm freindly again

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,27 @@
 /*   By: mratke <mratke@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 15:54:32 by mratke            #+#    #+#             */
-/*   Updated: 2024/10/18 15:24:14 by mratke           ###   ########.fr       */
+/*   Updated: 2024/10/18 15:45:48 by mratke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+static int	ft_printf_format_definer(const char *format, int i, va_list args)
+{
+	int	printed_len;
+
+	printed_len = 0;
+	if (format[i] == 'c')
+		printed_len += ft_printf_char((char)va_arg(args, int));
+	else if (format[i] == 's')
+		printed_len += ft_printf_str(va_arg(args, char *));
+	else if (format[i] == 'i' || format[i] == 'd')
+		printed_len += ft_printf_int(va_arg(args, int));
+	else if (format[i] == 'u')
+		printed_len += ft_printf_uint(va_arg(args, unsigned int));
+	return (printed_len);
+}
 
 int	ft_printf(const char *format, ...)
 {
@@ -26,14 +42,7 @@ int	ft_printf(const char *format, ...)
 		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
-			if (format[i] == 'c')
-				printed_len += ft_printf_char((char)va_arg(args, int));
-			else if (format[i] == 's')
-				printed_len += ft_printf_str(va_arg(args, char *));
-			else if (format[i] == 'i' || format[i] == 'd')
-				printed_len += ft_printf_int(va_arg(args, int));
-			else if (format[i] == 'u')
-				printed_len += ft_printf_uint(va_arg(args, unsigned int));
+			printed_len += ft_printf_format_definer(format, i, args);
 		}
 		else
 			printed_len += ft_printf_char(format[i]);
